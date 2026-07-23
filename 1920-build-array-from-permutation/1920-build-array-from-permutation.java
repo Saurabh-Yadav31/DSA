8d509783c9883adc268991d440de49f35b7478e4
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-07-23 22:35:57 +0530

    Time: 1 ms (99.47%), Space: 46.9 MB (88.1%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] buildArray(int[] nums) {
 
-        int[] ans = new int[nums.length];
+        int[] ans = new int[nums.length]; //By creating a answer array as a brute force approach
 
         for(int i = 0; i < nums.length; i++){
             ans[i] = nums[nums[i]];
@@ -9,3 +9,24 @@ class Solution {
         return ans;
     }
 }
+
+/*Optimized approach
+class Solution {
+    public int[] buildArray(int[] nums) {
+
+        int n = nums.length;
+
+        // Store both old and new values
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n;
+        }
+
+        // Extract only the new values
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] / n;
+        }
+
+        return nums;
+    }
+}
+*/
