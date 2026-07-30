0ec6b7f564b58f90907b06a36c2a275a68f5b712
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-07-30 23:01:32 +0530

    Time: 1 ms (99.96%), Space: 71.3 MB (60.25%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -32,3 +32,80 @@ class Solution {
         return n + 1;
     }
 }
+
+/* Other two initial solution which I dervied
+Algorithm:
+// 1. Start with target = 1.
+// 2. Traverse the entire array to check whether target exists.
+// 3. If found, increment target by 1.
+// 4. Again traverse the entire array for the new target.
+// 5. Repeat until a target is not found.
+// 6. Return that target as the first missing positive.
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int target = 1;
+        while (true) {
+            boolean found = false;
+
+            for (int num : nums) {
+                if (num == target) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return target;
+            }
+            target++;
+        }
+    }
+}
+
+Complexity
+Time: O(n²)
+Space: O(1)
+---------------------------------------------------------
+
+2. Algorithm
+// 1. Sort the array.
+// 2. Initialize target = 1.
+// 3. Traverse the sorted array.
+// 4. Ignore negative numbers and zero.
+// 5. Ignore duplicate numbers.
+// 6. If current number equals target,
+//    increment target.
+// 7. If current number is greater than target,
+//    target is the first missing positive.
+// 8. Return target.
+
+import java.util.Arrays;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        Arrays.sort(nums);
+        int target = 1;
+
+        for (int num : nums) {
+
+            if (num <= 0) {
+                continue;
+            }
+            if (num == target) {
+                target++;
+            }
+            else if (num > target) {
+                return target;
+            }
+        }
+        return target;
+    }
+}
+
+Complexity
+Time: O(n log n)
+Space: O(1) (ignoring the internal stack space used by Java's sort)
+*/
+
