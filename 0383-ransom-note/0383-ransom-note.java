12e6f593f000e8caa3c33e2f798dd58ad346d24c
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-08-12 23:25:18 +0530

    Time: 1 ms (99.95%), Space: 46.1 MB (95.55%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -18,3 +18,12 @@ class Solution {
         return true;  
     }
 }
+
+// Algorithm: (Similar to how I solved valid anagram)
+// 1. Create an integer array of size 26 to store character frequencies.
+// 2. Traverse the magazine string and increase the count of each character.
+// 3. Traverse the ransomNote string.
+// 4. For each character, decrease its available count.
+// 5. If the count becomes negative, return false because the magazine
+//    does not contain enough of that character.
+// 6. If the entire ransomNote is processed successfully, return true.
