55967bf5112cc76dcaa418e6fef04dd39f2a9ef7
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-08-20 21:31:31 +0530

    Time: 1 ms (83.64%), Space: 42.5 MB (88.2%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -6,27 +6,22 @@ class Solution {
         if (pattern.length() != words.length) {
             return false;
         }
-
         HashMap<Character, String> mapPW = new HashMap<>();
         HashMap<String, Character> mapWP = new HashMap<>();
 
         for (int i = 0; i < pattern.length(); i++) {
-
             char p = pattern.charAt(i);
             String word = words[i];
 
             if (mapPW.containsKey(p) && !mapPW.get(p).equals(word)) {
                 return false;
             }
-
             if (mapWP.containsKey(word) && mapWP.get(word) != p) {
                 return false;
             }
-
             mapPW.put(p, word);
             mapWP.put(word, p);
         }
-
         return true;
     }
 }
