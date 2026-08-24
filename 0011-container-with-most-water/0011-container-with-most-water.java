b77bd81d31383565ae211d9f355ee15952724176
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-08-24 22:24:46 +0530

    Time: 5 ms (83.14%), Space: 77.5 MB (29.51%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,7 +1,5 @@
 class Solution {
     public int maxArea(int[] height) {
-        
-
         int left = 0;
         int right = height.length - 1;
         int maxArea = 0;
@@ -20,7 +18,6 @@ class Solution {
                 right--;
             }
         }
-
         return maxArea;
     }
 }
