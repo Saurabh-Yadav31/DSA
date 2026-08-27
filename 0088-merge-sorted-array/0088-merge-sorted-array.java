1e103c76fd6a1918cf8299ca3fe722d58a6ec4b2
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-08-27 23:14:34 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,7 +6,6 @@ class Solution {
         int k = m + n - 1;
 
         while (i >= 0 && j >= 0) {
-
             if (nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
@@ -14,10 +13,8 @@ class Solution {
                 nums1[k] = nums2[j];
                 j--;
             }
-
             k--;
         }
-
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
@@ -25,3 +22,13 @@ class Solution {
         }
     }
 }
+// Algorithm:
+// 1. Set i = m - 1, j = n - 1, and k = m + n - 1.
+// 2. Traverse from the end while i >= 0 and j >= 0.
+// 3. Compare nums1[i] and nums2[j].
+// 4. Place the larger element at nums1[k].
+// 5. Move the pointer of the element that was placed and decrement k.
+// 6. If elements of nums2 are still remaining, copy them into nums1.
+// 7. The array is now merged in sorted order.
+// Time Complexity: O(m + n)
+// Space Complexity: O(1)
