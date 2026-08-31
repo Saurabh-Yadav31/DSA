54128ebb2411422785c7e87fff2ca093892fcabd
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-08-31 23:09:41 +0530

    Time: 2 ms (73.26%), Space: 42.9 MB (26.96%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -15,3 +15,15 @@ class Solution {
         return i == s.length();
     }
 }
+/*
+1. If length of s > length of t, return false.
+2. Initialize i = 0 and j = 0.
+3. Traverse both strings using i and j.
+4. If s[i] == t[j], move both i and j forward.
+5. Otherwise, move only j forward.
+6. Continue until either string is completely traversed.
+7. If i reaches the end of s, return true.
+8. Otherwise, return false.
+Time Complexity: O(n) where n is the length of t
+Space Complexity: O(1)
+*/
