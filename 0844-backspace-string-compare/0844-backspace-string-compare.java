439a18ac8fc902ecd460df634d7c70e7a84199cc
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-04 22:37:24 +0530

    Time: 0 ms (100%), Space: 43 MB (67.22%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,5 +1,81 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            // Find next valid character in s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            // Find next valid character in t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            // One string has a valid character, the other doesn't
+            if ((i >= 0) != (j >= 0)) {
+                return false;
+            }
+            // Both have valid characters, but they are different
+            if (i >= 0 && s.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        return true;
+    }
+}
+/* Optimal solution using pointers
+1. Set i = s.length() - 1
+   Set j = t.length() - 1
+2. Set skipS = 0 and skipT = 0.
+3. Move i backwards:
+   - If s[i] == '#', increase skipS.
+   - If skipS > 0, skip the current character and decrease skipS.
+   - Otherwise, stop because s[i] is a valid character.
+4. Do the same for j in t.
+5. If one string has a valid character and the other doesn't,
+   return false.
+6. If both have valid characters and they are different,
+   return false.
+7. Move both pointers one position left.
+8. Repeat until both strings are completely processed.
+9. Return true.
+Time:  O(n + m)
+Space: O(1)
+*/
+
+
+
+
+
+
+
+
+/* Brute force approach
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
         StringBuilder s1 = buildString(s);
         StringBuilder t1 = buildString(t);
 
@@ -22,3 +98,14 @@ class Solution {
         return sb;
     }
 }
+1. Create a StringBuilder for each string.
+2. Traverse the string character by character.
+3. If the character is not '#', add it to the StringBuilder.
+4. If the character is '#':
+   - If the StringBuilder is not empty, remove its last character.
+5. Do the same for both strings.
+6. Compare the two resulting strings.
+7. If they are equal, return true; otherwise return false.
+Time:  O(n + m)
+Space: O(n + m)
+*/
