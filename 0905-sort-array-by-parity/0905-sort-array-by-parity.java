1b7c613e82df8c3f6bf35e014e2bd9876e317930
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-08 21:39:51 +0530

    Time: 0 ms (100%), Space: 46.6 MB (84.75%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -20,6 +20,21 @@ class Solution {
     }
 }
 
+/*
+Algorithm: Two Pointers approach
+1. Set i = 0 and j = nums.length - 1.
+2. While i < j:
+   a. If nums[i] is even:
+      move i forward.
+   b. If nums[j] is odd:
+      move j backward.
+   c. If nums[i] is odd AND nums[j] is even:
+      swap nums[i] and nums[j].
+      Move both pointers.
+3. Return nums.
+Time:  O(n)
+Space: O(n)
+*/
 
 
 
