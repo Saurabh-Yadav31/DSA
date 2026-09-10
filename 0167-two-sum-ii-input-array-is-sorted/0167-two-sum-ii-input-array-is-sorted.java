097650614eba2baa5cdf245427db04a11bc79ba0
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-10 23:19:43 +0530

    Time: 2 ms (95.8%), Space: 48.5 MB (23.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -17,3 +17,25 @@ class Solution {
         return new int[]{};
     }
 }
+/* Algorithm: Using Two pointers Appraoch
+1. Initialize two pointers:
+   - left = 0
+   - right = numbers.length - 1
+2. While left < right:
+   - Calculate:
+     sum = numbers[left] + numbers[right]
+3. If sum == target:
+   - Return the 1-based indices:
+     [left + 1, right + 1]
+4. If sum < target:
+   - We need a larger sum.
+   - Move left forward:
+     left++
+5. If sum > target:
+   - We need a smaller sum.
+   - Move right backward:
+     right--
+6. The problem guarantees exactly one solution.
+Time Complexity:  O(n)
+Space Complexity: O(1)
+*/
