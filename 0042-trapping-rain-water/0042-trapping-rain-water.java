66a57ab39b84b7bddf77cd6e159a54167f623799
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-11 22:27:11 +0530

    Time: 0 ms (100%), Space: 47.5 MB (89.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -30,3 +30,30 @@ class Solution {
         return water;
     }
 }
+/* Algorithm: Using two pointers aproach
+1. Initialize two pointers:
+   left = 0
+   right = n - 1
+2. Initialize the maximum boundary seen from each side:
+   leftMax = 0
+   rightMax = 0
+3. Initialize:
+   water = 0
+4. While left < right:
+   a. If height[left] <= height[right]:
+      - If height[left] >= leftMax:
+          update leftMax = height[left]
+      - Otherwise:
+          water += leftMax - height[left]
+      - Move left forward.
+
+   b. Otherwise:
+      - If height[right] >= rightMax:
+          update rightMax = height[right]
+      - Otherwise:
+          water += rightMax - height[right]
+      - Move right backward.
+5. Return water.
+Time Complexity:  O(n)
+Space Complexity: O(1)
+*/
