e89f3d46a98cac67ec6770b25de0ac6fd6458f54
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-12 22:26:27 +0530

    Time: 2 ms (99.85%), Space: 69.8 MB (10.48%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -19,3 +19,17 @@ class Solution {
         return (double) maxSum / k;
     }
 }
+/* Algorithm :+ Using Sliding Window
+1. Take a window of size k.
+2. Calculate the sum of the first k elements.
+3. Store this sum as the current maximum sum.
+4. Move the window one position at a time:
+   - Subtract the element leaving the window.
+   - Add the new element entering the window.
+5. After each movement, compare the new window sum with maxSum.
+6. Update maxSum if the new sum is larger.
+7. After checking all windows, divide maxSum by k.
+8. Return the maximum average.
+Time Complexity:  O(n)
+Space Complexity: O(1)
+*/
