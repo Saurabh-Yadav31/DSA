4bd0856b73a2499e00df65a7f22cd05939f0adaa
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-22 23:05:25 +0530

    Time: 3 ms (95.9%), Space: 52.3 MB (17.73%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -19,3 +19,16 @@ class Solution {
         return maxLength;
     }
 }
+/*Algorithm: Using Sliding Window
+1.Initialize left = 0, zeroCount = 0, and maxLength = 0.
+2.Traverse the array using right from 0 to n - 1.
+3.If nums[right] == 0, increment zeroCount.
+4.If zeroCount > k, shrink the window from the left:
+4(a)If nums[left] == 0, decrement zeroCount.
+4(b)Move left forward.
+5.Calculate the current window length as right - left + 1.
+6.Update maxLength with the maximum window length.
+7.Return maxLength.
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
