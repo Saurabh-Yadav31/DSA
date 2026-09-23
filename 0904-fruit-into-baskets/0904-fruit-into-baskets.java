1db6553f0eb3be1444e4e114fce817293b952140
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-23 21:48:05 +0530

    Time: 53 ms (77.78%), Space: 70.6 MB (29.79%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -21,3 +21,18 @@ class Solution {
         return maxLength;
     }
 }
+/*
+1.Initialize left = 0, maxLength = 0, and a HashMap to store the frequency of each fruit in the current window.
+2.Traverse the array using right.
+3.Add fruits[right] to the HashMap and increase its frequency.
+4.If the HashMap contains more than 2 distinct fruit types:
+4(a).Decrease the frequency of fruits[left].
+4(b).If its frequency becomes 0, remove it from the HashMap.
+4(b).Move left forward.
+5.Calculate the current window length using right - left + 1.
+6.Update maxLength with the maximum window length.
+7.Return maxLength.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
