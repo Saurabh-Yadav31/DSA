538d5a242e17a4fdbf9837b23452768b36d92f56
Saurabh-Yadav31 <saurabhyadav3101@gmail.com>
2026-09-23 21:42:21 +0530

    Time: 7 ms (89.33%), Space: 46.2 MB (64.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -20,3 +20,20 @@ class Solution {
         return maxLength;
     }
 }
+/* Algorithm: Varaiable Sliding Window
+1.Initialize left = 0, maxFrequency = 0, and maxLength = 0.
+2.Create a frequency array to store the count of each character in the current window.
+3.Traverse the string using right.
+4.Add the current character to the frequency array.
+5.Update maxFrequency with the highest frequency of any character in the current window.
+6.Calculate the number of replacements needed:
+    window length - maxFrequency
+7.If the required replacements are greater than k, shrink the window:
+    7(a)Decrease the frequency of the character at left.
+    7(b)Move left forward.
+8.Calculate the current valid window length and update maxLength.
+9.Return maxLength.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
